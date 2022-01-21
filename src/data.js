5d902f13eff77cb5b8f42ee35ea10fe5b67a0469
Manlangit <manlangitv@gmail.com>
2022-01-21 11:05:25 -0500

    added images and edit data.js

--- src/data.js
+++ src/data.js
@@ -6,26 +6,31 @@ export const products = [
    },
    {
       id: 2,
+      img: 'https://github.com/AbrahamTesla/Contact_Organizer/blob/master/client/src/Image/register.png',
+      link: 'https://contactorganizermern.herokuapp.com/login',
+   },
+   {
+      id: 3,
       img: 'https://images.pexels.com/photos/2726370/pexels-photo-2726370.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       link: 'https://dazzling-jennings-e004a8.netlify.app/',
    },
    {
-      id: 3,
+      id: 4,
       img: 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=',
       link: 'https://abrahamtesla.github.io/FoodTacular/',
    },
    {
-      id: 4,
+      id: 5,
       img: 'https://images.pexels.com/photos/37072/abraham-lincoln-lincoln-memorial-washington-dc-lincoln.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       link: 'https://famousfacesclickygame.herokuapp.com/',
    },
    {
-      id: 5,
+      id: 6,
       img: 'https://images.pexels.com/photos/697243/pexels-photo-697243.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       link: 'https://sleepy-crag-73379.herokuapp.com/',
    },
    {
-      id: 6,
+      id: 7,
       img: 'https://images.pexels.com/photos/2219024/pexels-photo-2219024.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       link: 'https://abrahamtesla.github.io/constructionSite/public/index.html',
    },
